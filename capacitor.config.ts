b3b4e9b4eb534928b16de16cf5bc7c539caad73d
Paul Baik <pbaik@ridgelineintl.com>
2023-01-06 15:34:26 -0500

    changes for android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'leafletIonic',
   appName: 'leafletIonic',
   webDir: 'www',
   bundledWebRuntime: false
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import * as Leaflet from 'leaflet';
+import { antPath } from 'leaflet-ant-path';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit, OnDestroy {
+  map: Leaflet.Map;
 
-  constructor() {}
+  constructor() { }
 
+  ngOnInit() { }
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    this.map = Leaflet.map('mapId').setView([28.644800, 77.216721], 5);
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © Angular LeafLet',
+    }).addTo(this.map);
+
+    Leaflet.marker([28.6, 77]).addTo(this.map).bindPopup('Delhi').openPopup();
+    Leaflet.marker([34, 77]).addTo(this.map).bindPopup('Leh').openPopup();
+    Leaflet.marker([20, 90]).addTo(this.map).bindPopup('Bah').openPopup();
+
+    antPath([[28.644800, 77.216721], [34.1526, 77.5771], [20, 90]],
+      { color: '#FF0000', weight: 5, opacity: 0.6 })
+      .addTo(this.map);
+  }
+
+  /** Remove map when we have multiple map object */
+  ngOnDestroy() {
+    this.map.remove();
+  }
 }
